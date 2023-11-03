f650ef2d9a7386444d90c169d609c59930e76e6f
Gongme <k102kg12@naver.com>
2023-11-04 04:44:58 +0900

    Feat: 자유게시판 반응형 디자인 추가

--- animation-frontend/src/pages/animation/FreeBoard.js
+++ animation-frontend/src/pages/animation/FreeBoard.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
-import { Button, Row, Table } from 'react-bootstrap';
 import { useEffect } from 'react';
-import Page from '../../components/Page';
 import store from '../../Redux/store';
 import { setFreeBoards } from '../../Redux/action';
 import { useSelector } from 'react-redux';
+
+import {
+  CustomTable,
+  Th1,
+  Th2,
+  Th3,
+  Th4,
+  Th5,
+  Tr1,
+  Tr2,
+} from '../../styledcomponents/FreeBoard.styled';
 
 function FreeBoard() {
   const freeBoards = useSelector((state) => state.freeBoards);
@@ -19,10 +28,41 @@ function FreeBoard() {
 
   console.log(freeBoards);
 
+  function DateTime(fbDate) {
+    const date = new Date(fbDate);
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${month}/${day}/${hours}:${minutes}`;
+  }
+
   return (
     <>
-      <Table striped bordered hover></Table>
-      <Page></Page>
+      <h1>자유게시판</h1>
+
+      <CustomTable className="custom-table">
+        <thead>
+          <Tr1>
+            <Th1>NO</Th1>
+            <Th2>제목</Th2>
+            <Th3>글쓴이</Th3>
+            <Th4>작성일</Th4>
+            <Th5>조회수</Th5>
+          </Tr1>
+        </thead>
+        <tbody>
+          {freeBoards.map((data) => (
+            <Tr2 key={data.fbNum}>
+              <Th1>{data.fbNum}</Th1>
+              <Th2>{data.fbTitle}</Th2>
+              <Th3>{data.userid}</Th3>
+              <Th4>{DateTime(data.fbDate)}</Th4>
+              <Th5>{data.fbReadCount}</Th5>
+            </Tr2>
+          ))}
+        </tbody>
+      </CustomTable>
     </>
   );
 }
---
+++ animation-frontend/src/styledcomponents/FreeBoard.styled.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const CustomTable = styled.table`
+  border-collapse: separate;
+  width: 100%;
+`;
+
+export const Tr1 = styled.th`
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  line-height: 35px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  height: 35px;
+  text-align: center;
+  display: flex;
+  border-top: none;
+  font-size: 16px;
+  font-family: 'Noto Sans KR', sans-serif;
+  @media (max-width: 767px) {
+    font-size: 13px;
+  }
+
+  @media (min-width: 768px) and (max-width: 991px) {
+    font-size: 14px;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 16px;
+  }
+`;
+
+export const Tr2 = styled.th`
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  line-height: 35px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  height: 35px;
+  text-align: center;
+  display: flex;
+  border-top: none;
+  font-size: 13px;
+  font-family: 'Noto Sans KR', sans-serif;
+  @media (max-width: 767px) {
+    font-size: 10px;
+  }
+
+  @media (min-width: 768px) and (max-width: 991px) {
+    font-size: 11px;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 13px;
+  }
+`;
+
+export const Th1 = styled.th`
+  width: 3%;
+`;
+
+export const Th2 = styled.th`
+  width: 60%;
+`;
+
+export const Th3 = styled.th`
+  width: 15%;
+`;
+
+export const Th4 = styled.th`
+  width: 20%;
+`;
+
+export const Th5 = styled.th`
+  width: 10%;
+`;
