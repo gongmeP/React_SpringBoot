aadac90b9b7f343b2c9561dda52196a7e8d407a8
Gongme <k102kg12@naver.com>
2023-11-10 00:06:56 +0900

    Rename: AniDetail StyledComponent 분리

--- animation-frontend/src/pages/animation/Detail.js
+++ animation-frontend/src/pages/animation/Detail.js
@@ -3,7 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import {
+  AniImg,
+  P_Styled,
+  P_Styled2,
+  PlayDiv_Styled,
+  PlayImg_Styled,
+  StarImg,
+  StrongStyled,
+} from '../../styledcomponents/AniDetail.styled';
 
 function Detail(props) {
   const propsParam = useParams();
@@ -88,47 +96,5 @@ function Detail(props) {
     </>
   );
 }
-const AniImg = styled.img`
-  width: 100%;
-  height: auto;
-  border-radius: 0.3125rem;
-  margin-top: 0.3125rem;
-`;
-
-const StarImg = styled.img`
-  width: 1.5625rem;
-  height: 1.5625rem;
-  margin-bottom: 0.25rem;
-`;
-
-const StrongStyled = styled.strong`
-  height: 1.5625rem;
-  margin: 0;
-  padding: 0;
-  line-height: 1.5625rem;
-`;
-
-const P_Styled = styled.p`
-  height: 1.5625rem;
-  line-height: 1.5625rem;
-`;
-
-const P_Styled2 = styled.p`
-  font-size: 1rem;
-`;
-
-const PlayImg_Styled = styled.img`
-  height: 3.75rem;
-  width: 3.75rem;
-  cursor: pointer;
-`;
-
-const PlayDiv_Styled = styled.div`
-  height: 3.75rem;
-  width: 40%;
-  font-size: 1.25rem;
-  line-height: 3.75rem;
-  cursor: pointer;
-`;
 
 export default Detail;
---
+++ animation-frontend/src/styledcomponents/AniDetail.styled.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const AniImg = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 0.3125rem;
+  margin-top: 0.3125rem;
+`;
+
+export const StarImg = styled.img`
+  width: 1.5625rem;
+  height: 1.5625rem;
+  margin-bottom: 0.25rem;
+`;
+
+export const StrongStyled = styled.strong`
+  height: 1.5625rem;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5625rem;
+`;
+
+export const P_Styled = styled.p`
+  height: 1.5625rem;
+  line-height: 1.5625rem;
+`;
+
+export const P_Styled2 = styled.p`
+  font-size: 1rem;
+`;
+
+export const PlayImg_Styled = styled.img`
+  height: 3.75rem;
+  width: 3.75rem;
+  cursor: pointer;
+`;
+
+export const PlayDiv_Styled = styled.div`
+  height: 3.75rem;
+  width: 40%;
+  font-size: 1.25rem;
+  line-height: 3.75rem;
+  cursor: pointer;
+`;
