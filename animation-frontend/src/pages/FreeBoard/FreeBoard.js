94f8115ba5e38be7668b13786fd83c1b3a1f8b22
Gongme <k102kg12@naver.com>
2023-11-12 04:19:35 +0900

    Feat: 전체 게시글 목록 호출 버튼 추가

--- animation-frontend/src/pages/FreeBoard/FreeBoard.js
+++ animation-frontend/src/pages/FreeBoard/FreeBoard.js
@@ -8,9 +8,10 @@ import {
 import Board from '../../components/Board';
 import Page from '../../components/Page';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import BoradSerch from '../../components/BoardSerch';
+import { Button } from 'react-bootstrap';
 
 function FreeBoard() {
   const Pages = useSelector((state) => state.pages);
@@ -37,10 +38,21 @@ function FreeBoard() {
     TotalPage();
   }, []);
 
+  const boardlistgo = () => {
+    window.location.href = '/freeBoard';
+  };
+
   return (
     <>
       <Board></Board>
       <div style={{ display: 'flex', justifyContent: 'end' }}>
+        <Button
+          onClick={boardlistgo}
+          className="btn-primary mb-1"
+          style={{ marginRight: '20px', marginTop: '10px' }}
+        >
+          전체글
+        </Button>
         <Link
           to="/saveFreeBoard"
           className="btn btn-primary mb-1"
